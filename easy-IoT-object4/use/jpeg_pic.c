fb74e1a051b9d990467ee14bb845b7b9303d85e1
veis-lzf <1256639585@qq.com>
2023-04-17 18:50:28 +0800

    屏蔽掉调用操作

--- easy-IoT-object4/use/jpeg_pic.c
+++ easy-IoT-object4/use/jpeg_pic.c
@@ -1,9 +1,7 @@
 #include   "jpeg.h"
-#include   "jpeg2.h"
-#include   "jpeg3.h"
+//#include   "jpeg2.h"
+//#include   "jpeg3.h"
 #include   "drivers.h"
-
-
 
 
 #define PIC_6_BASE_ADDR	(2*1024*1024)   
@@ -13,26 +11,21 @@
 
 void show_pic(u8 *pData )    
 {
-     
- DrawRGBImage(0,0, 320,240,(u8 *)&pData);
-
+	DrawRGBImage(0,0, 320,240,(u8 *)&pData);
 }
 
 int show_pic_init(u8 pic)
 {
+	switch(pic)
+	{
+		case 1: 
+//			DrawRGBImage(0,0,480,320,(u8 *)&gImage_1[0]);
+		break;
 
-  switch(pic)
-  {
-      case 1:// DrawRGBImage(0,0,480,320,(u8 *)&gImage_1[0]);
-           break;
-      case 2:  DrawRGBImage(0,0,480,320,(u8 *)&gImage_2[0]);
-           break;
-  
-  }      
-   
-  
-   
-
-     return 0;
+		case 2:  
+//			DrawRGBImage(0,0,480,320,(u8 *)&gImage_2[0]);
+		break;
+	}
+	return 0;
 }
 
